eeaa739d7055e8ace6684cb0db6dbaad5ea418df
codeYann <codeyan10@gmail.com>
2021-01-20 15:05:42 -0300

    Aprendendo como declarar variáveis, comentários, e output

--- src/vars/index.jl
+++ src/vars/index.jl
@@ -1,5 +1,5 @@
-ω = 10
-θ = ω + 5
-δ = θ + ω
+# Definindo variaveis e testando a funcionalidade \nome
+ω = 10; θ = ω + 5; δ = θ + ω;
 
+# Printando valores para ω, θ, e ϵ
 println(ω, " ", θ, " ", δ);
